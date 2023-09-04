d443e4d92397f25bf2c44e7f6e11aca7594d8d45
Bakeradm6 <123577761+Bakeradm6@users.noreply.github.com>
2023-09-04 14:27:25 -0500

    added put route to tags

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -3,11 +3,11 @@ const { Tag, Product, ProductTag } = require('../../models');
 
 // The `/api/tags` endpoint
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all tags
   // be sure to include its associated Product data
   try {
-    let tags= Tag.findAll({
+    let tags= await Tag.findAll({
       include: [Product]
     })
   } catch(err) {
@@ -15,12 +15,12 @@ router.get('/', (req, res) => {
   }
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
   try {
     let id= req.params.id
-    let selectedTag= Tag.findByPk(id, {
+    let selectedTag= await Tag.findByPk(id, {
       include: [Product]
     })
   } catch(err) {
@@ -28,20 +28,24 @@ router.get('/:id', (req, res) => {
   }
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // create a new tag
   try {
-    let newTag= Tag.create(req.body)
+    let newTag= await Tag.create(req.body)
     res.json(newTag)
   } catch(err) {
     res.status(500).json(err)
   }
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a tag's name by its `id` value
   try {
-
+    let id= req.params.id
+    let updatedCategory = await Tag.update(req.body, {
+      where: id
+    })
+    res.json(updatedCategory)
   } catch(err) {
     res.status(500).json(err)
   }
